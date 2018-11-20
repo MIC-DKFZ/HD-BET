2b5b82c55121546b9cd0debc7e2a149780ab5f2b
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2018-11-20 17:00:51 +0100

    now install hd-bet tool to be used form anywhere. Automatically installs all dependencies except batchgenerators

--- HD_BET/run.py
+++ HD_BET/run.py
@@ -20,8 +20,8 @@ def apply_bet(img, bet, out_fname):
     sitk.WriteImage(out, out_fname)
 
 
-def run(mri_fnames, output_fnames, mode="accurate", config_file=os.path.join(HD_BET.__path__[0], "config.py"), device=0,
-        postprocess=False, do_tta=True, keep_mask=True):
+def run_hd_bet(mri_fnames, output_fnames, mode="accurate", config_file=os.path.join(HD_BET.__path__[0], "config.py"), device=0,
+               postprocess=False, do_tta=True, keep_mask=True):
     """
 
     :param mri_fnames: str or list/tuple of str
@@ -106,89 +106,3 @@ def run(mri_fnames, output_fnames, mode="accurate", config_file=os.path.join(HD_
             os.remove(mask_fname)
 
 
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-input', help='input. Can be either a single file name or an input folder. If file: must be '
-                                       'nifti (.nii.gz) and can only be 3D. No support for 4d images, use fslsplit to '
-                                       'split 4d sequences into 3d images. If folder: all files ending with .nii.gz '
-                                       'within that folder will be brain extracted.', required=True, type=str)
-    parser.add_argument('-output', help='output. Can be either a filename or a folder. If it does not exist, the folder'
-                                     ' will be created', required=True, type=str)
-    parser.add_argument('-mode', type=str, default='accurate', help='can be either \'fast\' or \'accurate\'. Fast will '
-                                                                'use only one set of parameters whereas accurate will '
-                                                                'use the five sets of parameters that resulted from '
-                                                                'our cross-validation as an ensemble. Default: '
-                                                                    'accurate',
-                        required=False)
-    parser.add_argument('-device', default='0', type=str, help='used to set on which device the prediction will run. '
-                                                               'Must be either int or str. Use int for GPU id or '
-                                                               '\'cpu\' to run on CPU. Default: 0',
-                        required=False)
-    parser.add_argument('-tta', default=1, required=False, type=int, help='whether to use test time data augmentation '
-                                                                          '(mirroring). 1= True, 0=False. Disable this '
-                                                                          'if you are using CPU to speed things up! '
-                                                                          'Default: 1')
-    parser.add_argument('-pp', default=0, type=int, required=False, help='set to 1 to enable postprocessing (remove all'
-                                                                         ' but the largest connected component in '
-                                                                         'the prediction. Default: 0')
-    parser.add_argument('-s', '--save_mask', default=0, type=int, required=False, help='if set to 1 the mask will be '
-                                                                                       'saved as well')
-
-    args = parser.parse_args()
-
-    input_file_or_dir = args.input
-    output_file_or_dir = args.output
-    mode = args.mode
-    device = args.device
-    tta = args.tta
-    pp = args.pp
-    save_mask = args.save_mask
-
-    params_file = os.path.join(HD_BET.__path__[0], "model_final.py")
-    config_file = os.path.join(HD_BET.__path__[0], "config.py")
-
-    assert os.path.abspath(input_file_or_dir) != os.path.abspath(output_file_or_dir), "output must be different from input"
-
-    if device == 'cpu':
-        pass
-    else:
-        device = int(device)
-
-    if os.path.isdir(input_file_or_dir):
-        maybe_mkdir_p(output_file_or_dir)
-        input_files = subfiles(input_file_or_dir, suffix='.nii.gz', join=False)
-
-        if len(input_files) == 0:
-            raise RuntimeError("input is a folder but no nifti files (.nii.gz) were found in here")
-
-        output_files = [os.path.join(output_file_or_dir, i) for i in input_files]
-        input_files = [os.path.join(input_file_or_dir, i) for i in input_files]
-    else:
-        if not output_file_or_dir.endswith('.nii.gz'):
-            output_file_or_dir += '.nii.gz'
-        output_files = [output_file_or_dir]
-        input_files = [input_file_or_dir]
-
-    if tta == 0:
-        tta = False
-    elif tta == 1:
-        tta = True
-    else:
-        raise ValueError("Unknown value for tta: %s. Expected: 0 or 1" % str(tta))
-
-    if pp == 0:
-        pp = False
-    elif pp == 1:
-        pp = True
-    else:
-        raise ValueError("Unknown value for pp: %s. Expected: 0 or 1" % str(pp))
-
-    if save_mask == 0:
-        save_mask = False
-    elif save_mask == 1:
-        save_mask = True
-    else:
-        raise ValueError("Unknown value for pp: %s. Expected: 0 or 1" % str(pp))
-
-    run(input_files, output_files, mode, config_file, device, pp, tta, save_mask)
--- setup.py
+++ setup.py
@@ -3,8 +3,24 @@ from setuptools import setup
 setup(name='HD_BET',
       version='1.0',
       description='Tool for brain extraction',
-      url='',  # TODO
+      url='',  # TODO,
+      python_requires='>=3.5',
       author='Fabian Isensee',
-      author_email='f.isensee@dkfz-heidelberg.de',
+      author_email='f.isensee@dkfz.de',
       license='MIT',  # TODO
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=[
+      'numpy',
+      'torch>=0.4.1',
+      'scikit-image',
+      'SimpleITK'
+      ],
+      scripts=['HD_BET/hd-bet'],
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering',
+          'Operating System :: Unix'
+      ]
+      )
+
